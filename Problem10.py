0add42b9da8230565aa7645305514e7e4c3fa1c1
i.izurza <i.izurza@consulpyme.com>
2024-01-23 16:24:55 +0100

    More

---
+++ Problem10.py
@@ -0,0 +1,12 @@
+from Problem7 import find_next_prime
+
+def primeSummation(n):
+    primes = [2,3]
+    while True:
+        next = find_next_prime(primes[-1])
+        if next>n: break
+        primes.append(next)
+    return sum(primes)
+
+    
+print(primeSummation(2000000))
