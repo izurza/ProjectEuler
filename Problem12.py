ba578ca2c3b4b1502c04c8399e513846ef7f6379
i.izurza <i.izurza@consulpyme.com>
2024-01-24 10:31:26 +0100

    optimized

--- Problem12.py
+++ Problem12.py
@@ -1,19 +1,26 @@
-
+import math
 def divisible_triangle_number(n):
     i=1
     while(True):
-        if factors(triangle_number(i))>=n:
-            return triangle_number(i)
+        tri = triangle_number(i)
+        if factors(tri)>=n:
+            return tri
         i+=1
-
 
 def triangle_number(n):
     return sum(range(n+1))
+
 def factors(n):
     i=0
-    for x in range(1,n+1):
+    for x in range(1,int(math.sqrt(n))+1):
+        print(x)
         if n%x==0:
-            i+=1
+            print(x*x!=n)
+            if x*x!=n: i+=2
+            else: i+=1
+            print(i)
+            # i+=2 if x*x != n else 1
     return i
 
-print(divisible_triangle_number(167))
+# print(divisible_triangle_number(167))
+print(factors(6))
