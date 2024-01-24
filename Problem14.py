030251679475bba85b0a98a800445e29da9d70c7
i.izurza <i.izurza@consulpyme.com>
2024-01-24 14:09:29 +0100

    some more

---
+++ Problem14.py
@@ -0,0 +1,23 @@
+
+def longest_collatz_sequence(n):
+    max_chain_number = 0
+    max_chain = 0
+    for x in range(n+1):
+        current = x
+        current_chain=0
+        while(current>1):
+            current = even_op(current) if is_even(current) else odd_op(current)
+            current_chain+=1
+        max_chain_number, max_chain = (x, current_chain) if current_chain > max_chain else (max_chain_number, max_chain)
+    return max_chain_number
+
+def even_op(n):
+    return n/2
+
+def odd_op(n):
+    return 3*n+1
+
+def is_even(n):
+    return n%2==0
+
+print(longest_collatz_sequence(5847))
---
+++ Problem15.py
@@ -0,0 +1,6 @@
+import math
+
+def lattice_paths(gridSize):
+    factorial_2n = math.factorial(2 * gridSize)
+    factorial_n = math.factorial(gridSize)
+    return factorial_2n // (factorial_n * factorial_n)
