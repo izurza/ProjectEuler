47b6dba1cd148df0fce14727ddcc3472a712f53f
izurza <izurza1994@gmail.com>
2024-02-07 10:35:16 +0100

    7/2/24/

---
+++ Problems21-30/Problem25.py
@@ -0,0 +1,13 @@
+## Problem 25
+
+def digit_fibonacci(n):
+    index=1
+    previous, current = 0, 1
+    if (n<=1):
+        return index
+    while (len(str(current))<n):
+        previous, current = current, previous + current
+        index += 1
+    return index
+
+print(digit_fibonacci(10))
